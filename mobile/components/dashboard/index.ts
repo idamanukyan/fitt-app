5c2aa5907744dbec16cb5ba58d1a0c5ffb8e3da1
imanukyan <idamyan01@gmail.com>
2026-05-11 20:46:04 +0200

    feat(dashboard): create dashboard component directory with barrel exports

---
+++ mobile/components/dashboard/index.ts
@@ -0,0 +1,8 @@
+export { DashboardHeader } from './DashboardHeader';
+export { TodayActivityCard } from './TodayActivityCard';
+export { NextWorkoutCard } from './NextWorkoutCard';
+export { QuickActionsRow } from './QuickActionsRow';
+export { WeeklyProgressCard } from './WeeklyProgressCard';
+export { BodyStatsCard } from './BodyStatsCard';
+export { AchievementsCard } from './AchievementsCard';
+export { DailyInsightCard } from './DailyInsightCard';
